352e517ba1644ded82e31536c32ab4ebd16507b3
Zahid Ronty <ronty@quick.no>
2022-04-24 23:38:44 +0200

    css refactored

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -16,7 +16,9 @@ export default function Article(){
     return (
         <React.Fragment>
             <div className={styles.container}>
-                <h1 className="title">Editor </h1>
+                <div className={styles.titleContainer}></div>
+                <h1 className="title"> Article Editor </h1>
+                <h3 className="sub_title">Click the title to edit</h3>
                 {isLoading && isFetching ? <Spinner/>: articlesData.map((articlesRow, index) => <FlexRow key={index} row={articlesRow} />)}
                 {error && <p>Error: {error.message}</p>}
             </div>
